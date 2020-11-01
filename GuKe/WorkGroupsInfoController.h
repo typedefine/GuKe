ae762bc3f4b78f712dadb94235ec16eaef944967
Jackson <3188919931@qq.com>
2020-11-01 23:06:19 +0800

    文件移动

---
+++ GuKe/WorkGroupsInfoController.h
@@ -0,0 +1,17 @@
+//
+//  WorkGroupsInfoController.h
+//  GuKe
+//
+//  Created by yb on 2020/11/1.
+//  Copyright © 2020 shangyukeji. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WorkGroupsInfoController : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
